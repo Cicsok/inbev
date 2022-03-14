89a0e85952b32c74bad3cb0b58af663b11b283d7
Gabor Ivanyi-Nagy <gabilampard@gmail.com>
2022-03-14 19:46:14 +0100

    test

--- public/browser-native-back-and-forward-arrows.js
+++ public/browser-native-back-and-forward-arrows.js
@@ -1,3 +1,6 @@
-window.onpopstate = function() {
+window.onpopstate = function(event) {
+
     alert("clicked back button");
+    alert(event.state);
+    alert(event);
  }; history.pushState({}, '');
