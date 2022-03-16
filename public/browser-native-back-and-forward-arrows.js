241d0c1d1d71a0e588523348c699ff3943431ebf
SzakacsAgi <szakacs.agi21@gmail.com>
2022-03-16 19:16:43 +0100

    Test01

---
+++ public/browser-native-back-and-forward-arrows.js
@@ -0,0 +1,18 @@
+window.onpopstate = function(event) {
+
+    alert("clicked button");
+
+    doSomething(event);
+ };
+
+ function doSomething(event) {
+    let factory = new NavigatorEventListener();
+
+   //  let platformType = identifyPlatformType();
+   //  let navigator = factory.create(platformType);
+   let currentSlug = window.location.pathname.replace('/', '');
+    factory.urlRewriter(currentSlug);
+
+    let platformSynchronizer = PlatformSynchronizer.createInstance();
+    platformSynchronizer.sync(event.state.replace('/', ''), platformType);
+ }
