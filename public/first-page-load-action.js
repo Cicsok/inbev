11dcab261981ac743ba6779035bf0f6200fa3a7a
Gabor Ivanyi-Nagy <gabilampard@gmail.com>
2022-03-28 21:02:54 +0200

    Test replaceState

--- public/first-page-load-action.js
+++ public/first-page-load-action.js
@@ -1,7 +1,7 @@
 window.onload = function(event) {
-       let slug = window.location.pathname;
-       let pagePath = siteConfig.pageToURLMapping[slug];
-       let state = { pagePath: pagePath};
+    let slug = window.location.pathname;
+    let pagePath = siteConfig.pageToURLMapping[slug];
+    let state = { pagePath: pagePath};
 
-       window.history.replaceState(state, document.title, pagePath);
+    window.history.replaceState(state, document.title, pagePath);
 };
