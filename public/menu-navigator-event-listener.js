b551fc62120af7201dbf4be7d783719bf436151f
Gabor Ivanyi-Nagy <gabilampard@gmail.com>
2022-03-28 20:50:58 +0200

    Test replaceState

--- public/menu-navigator-event-listener.js
+++ public/menu-navigator-event-listener.js
@@ -46,38 +46,13 @@ class NavigatorEventListener{
 
     }
 
-     urlRewriterForBackArrow(slug){
-          let pagePath = siteConfig.pageToURLMapping[slug];
+    addActiveLinkToNewActivePageOnDesktop(newActivePageSlug){
+        document.getElementsByClassName(newActivePageSlug + '-page-link')[0].classList.add(this.activeLinkClassName);
+    }
 
-          document.getElementById('specific-content').innerHTML = '';
-          document.getElementById('specific-content').appendChild(this.loadPage(window.location.origin + pagePath));
-
-          let state = { pagePath: pagePath};
-          window.history.replaceState(state, document.title, pagePath);
-        }
-
-
-     navigateForBackArrow(slug){
-         this.urlRewriterForBackArrow(slug);
-
-         document.getElementsByClassName(this.activeLinkClassName)[0].classList.remove(this.activeLinkClassName);
-
-         let searchPage = siteConfig.URLToPageMapping[slug];
-         let newActivePageSlug = Object.keys(siteConfig.header.pages).find(key => key == searchPage);
-
-         let platform = identifyPlatformType();
-         platform == 'DESKTOP'
-         ? this.addActiveLinkToNewActivePageOnDesktop(newActivePageSlug)
-         : this.addActiveLinkToNewActivePageOnMobile(newActivePageSlug);
-        }
-
-     addActiveLinkToNewActivePageOnDesktop(newActivePageSlug){
-         document.getElementsByClassName(newActivePageSlug + '-page-link')[0].classList.add(this.activeLinkClassName);
-     }
-
-     addActiveLinkToNewActivePageOnMobile(newActivePageSlug){
-         document.getElementsByClassName(newActivePageSlug + '-page-link-mobile')[0].classList.add(this.activeLinkClassName);
-     }
+    addActiveLinkToNewActivePageOnMobile(newActivePageSlug){
+        document.getElementsByClassName(newActivePageSlug + '-page-link-mobile')[0].classList.add(this.activeLinkClassName);
+    }
 
     loadPage(href){
         let xmlhttp = new XMLHttpRequest();
