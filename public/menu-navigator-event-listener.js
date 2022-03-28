f8660823973c8bacfe491e40b4c0ade2df5195cc
Gabor Ivanyi-Nagy <gabilampard@gmail.com>
2022-03-28 20:14:39 +0200

    Test replaceState

--- public/menu-navigator-event-listener.js
+++ public/menu-navigator-event-listener.js
@@ -30,60 +30,8 @@ class NavigatorEventListener{
 
     }
 
-    addActiveLinkToNewActivePageOnDesktop(newActivePageSlug){
-        document.getElementsByClassName(newActivePageSlug + '-page-link')[0].classList.add(this.activeLinkClassName);
-    }
-
-    addActiveLinkToNewActivePageOnMobile(newActivePageSlug){
-        document.getElementsByClassName(newActivePageSlug + '-page-link-mobile')[0].classList.add(this.activeLinkClassName);
-    }
-
     navigate(slug){
         this.urlRewriter(slug);
-   
-        document.getElementsByClassName(this.activeLinkClassName)[0].classList.remove(this.activeLinkClassName);
-
-        let slugAndLinkPair = siteConfig.header.pages;
-        let newActivePageSlug = Object.keys(slugAndLinkPair).find(key => key == slug);
-        
-        console.log(slugAndLinkPair);
-        console.log(newActivePageSlug);
-
-        let platform = identifyPlatformType();
-        platform == 'DESKTOP' 
-        ? this.addActiveLinkToNewActivePageOnDesktop(newActivePageSlug)
-        : this.addActiveLinkToNewActivePageOnMobile(newActivePageSlug);
-    }
-
-    loadPage(href){
-        let xmlhttp = new XMLHttpRequest();
-        xmlhttp.open("GET", href, false);
-        xmlhttp.send();
-        let parser = new DOMParser();
-        let responseDoc = parser.parseFromString (xmlhttp.responseText, "text/html");
-        let specificContent = responseDoc.getElementById('specific-content');
-        return specificContent;
-    }
-
-
-
-
-
-
-
-    urlRewriterForBackArrow(slug){
-        let pagePath = siteConfig.pageToURLMapping[slug];
-
-        document.getElementById('specific-content').innerHTML = '';
-        document.getElementById('specific-content').appendChild(this.loadPage(window.location.origin + pagePath));
-
-        let state = { pagePath: pagePath};
-        window.history.replaceState(state, document.title, pagePath);
-    }
-
-
-    navigateForBackArrow(slug){
-        this.urlRewriterForBackArrow(slug);
    
         document.getElementsByClassName(this.activeLinkClassName)[0].classList.remove(this.activeLinkClassName);
 
@@ -94,6 +42,49 @@ class NavigatorEventListener{
         platform == 'DESKTOP' 
         ? this.addActiveLinkToNewActivePageOnDesktop(newActivePageSlug)
         : this.addActiveLinkToNewActivePageOnMobile(newActivePageSlug);
+    }
+
+     urlRewriterForBackArrow(slug){
+          let pagePath = siteConfig.pageToURLMapping[slug];
+
+          document.getElementById('specific-content').innerHTML = '';
+          document.getElementById('specific-content').appendChild(this.loadPage(window.location.origin + pagePath));
+
+          let state = { pagePath: pagePath};
+          window.history.replaceState(state, document.title, pagePath);
+        }
+
+
+     navigateForBackArrow(slug){
+         this.urlRewriterForBackArrow(slug);
+
+         document.getElementsByClassName(this.activeLinkClassName)[0].classList.remove(this.activeLinkClassName);
+
+         let searchPage = siteConfig.URLToPageMapping[slug];
+         let newActivePageSlug = Object.keys(siteConfig.header.pages).find(key => key == searchPage);
+
+         let platform = identifyPlatformType();
+         platform == 'DESKTOP'
+         ? this.addActiveLinkToNewActivePageOnDesktop(newActivePageSlug)
+         : this.addActiveLinkToNewActivePageOnMobile(newActivePageSlug);
+        }
+
+     addActiveLinkToNewActivePageOnDesktop(newActivePageSlug){
+         document.getElementsByClassName(newActivePageSlug + '-page-link')[0].classList.add(this.activeLinkClassName);
+     }
+
+     addActiveLinkToNewActivePageOnMobile(newActivePageSlug){
+         document.getElementsByClassName(newActivePageSlug + '-page-link-mobile')[0].classList.add(this.activeLinkClassName);
+     }
+
+    loadPage(href){
+        let xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", href, false);
+        xmlhttp.send();
+        let parser = new DOMParser();
+        let responseDoc = parser.parseFromString (xmlhttp.responseText, "text/html");
+        let specificContent = responseDoc.getElementById('specific-content');
+        return specificContent;
     }
 }
 
--- public/siteConfig.js
+++ public/siteConfig.js
@@ -46,6 +46,11 @@ const siteConfig = {
       "/about-me": "about-me",
       "/reference-works": "reference-works",
       "/contact": "contact",
+
+       "home-page": "home-page",
+       "about-me": "about-me",
+       "reference-works": "reference-works",
+       "contact": "contact",
     },
 
   activeSliderImageName: "image1.jpg",
