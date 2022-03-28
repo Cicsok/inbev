5ca495cbbcb7e55de4266772d732484b04e9b8c8
Gabor Ivanyi-Nagy <gabilampard@gmail.com>
2022-03-28 18:50:18 +0200

    Test replaceState

--- public/page-load-actions.js
+++ public/page-load-actions.js
@@ -12,4 +12,16 @@ function test() {
     console.log(pagePath);
 }
 
-window.onload = test;
+window.onload = function(event) {
+       let slug = window.location.pathname;
+       let pagePath = siteConfig.pageToURLMapping[slug];
+       let hash = Math.random().toString(36).substr(2, 5);
+
+       let state = { slug: pagePath, hash: hash};
+                   //window.history.replaceState(state, document.title, slug);
+       window.history.replaceState(state, document.title, slug);
+
+       console.log("test");
+       console.log(slug);
+       console.log(pagePath);
+};
